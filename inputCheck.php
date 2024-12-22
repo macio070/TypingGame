e5a26d1312e9f78dc1d0943277a3912cade08ac0
macio070 <maciekgdowski@gmail.com>
2024-12-22 18:46:02 +0100

    v1.0.1

--- inputCheck.php
+++ inputCheck.php
@@ -5,6 +5,8 @@
     </head>
     <body>
     <?php
+    $url = "http://" . $_SERVER['SERVER_NAME'] . "/TypingGame";
+    header("Refresh: 1; URL=$url");
     $word = $_POST["random-word"];
     $user_input = $_POST["user-input"];
     $user_input = strtolower(trim($user_input));
